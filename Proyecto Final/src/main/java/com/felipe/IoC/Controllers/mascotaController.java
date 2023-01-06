51fe13c03d7d8103d49ba2f32697fd0d1f2bc969
Janesarai <saraipalaciosjane@gmail.com>
2023-01-05 22:16:55 -0600

    estructura mascotacontrolle

--- Proyecto Final/src/main/java/com/felipe/IoC/Controllers/mascotaController.java
+++ Proyecto Final/src/main/java/com/felipe/IoC/Controllers/mascotaController.java
@@ -1,33 +1,65 @@
 package com.felipe.IoC.Controllers;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.felipe.IoC.Models.Mascota;
-import com.felipe.IoC.Models.Publicacion;
-import com.felipe.IoC.Services.MascotaService;
-import com.felipe.IoC.Services.PublicacionService;
-import com.felipe.IoC.Services.UserService;
 
-@Controller
-public class mascotaController {
-    private final PublicacionService publicacionService;
-    private final MascotaService mascotaService;
+
     private final UserService userService;
 
+    @GetMapping("/mis-mascotas")
     public mascotaController(PublicacionService publicacionService, MascotaService mascotaService, UserService userService){
         this.publicacionService = publicacionService;
         this.mascotaService = mascotaService;
         this.userService = userService;
+
+        //Ahi tu lo haces funcionar
+        //Esta es una estructura basica de controladores para base de datos
+        //Listar
+        //Visualizar
+        //Agregar
+        //Modificar
+        //Eliminar
+
+        @GetMapping("")
+        public String mascotasTodas(){ //GET (Mostrar todas las mascotas asociadas al usuario)
+            return "home2.jsp"; 
+        }
+
+        @GetMapping("/ver/{id_mascota}") //GET (Mostrar una mascota en especifico)
+        public String visualizar(){
+            return "home2.jsp";
+        }
+
+        @GetMapping("/agregar") //GET (Mostrar formulario de agregar)
+        public String agregar(){
+            return "home2.jsp";
+        }
+
+        @PostMapping("/agregar") //POST (Agregar a base de datos)
+        public String visualizar(){
+            return "home2.jsp";
+        }
+
+        @GetMapping("/modificar/{id_mascota}") //GET (Desplegar formulario de modificacion)
+        public String visualizar(){
+            return "home2.jsp";
+        }
+
+        @PostMapping("/modificar/{id_mascota}") //POST (Modificacion de una mascota en bd)
+        public String visualizar(){
+            return "home2.jsp";
+        }
+
+        @PostMapping("/eliminar/{id_mascota}") //POST (Eliminacion de una mascota,tiene q apreecer una notificacioln)
+        public String visualizar(){
+            return "home2.jsp";
+        }
+
 }
+
 //para crear mascota por post
     @PostMapping("/publicacion")
     public Mascota crearPublicacion(Mascota mascota){
